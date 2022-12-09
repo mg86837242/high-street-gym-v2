ea84286cc2392d57b4dabf2695518ba85738de64
by151709129 <mg86837242@protonmail.com>
2022-12-09 23:25:40 +1000

    prettier formatting finalized

--- src/data/models/__models_sail_away__/logins.js
+++ src/data/models/__models_sail_away__/logins.js
@@ -1,33 +1,28 @@
-import { db_conn } from "../database.js";
+import { db_conn } from '../database.js';
 
 export function getLoginByID(login_id) {
-  return db_conn.query("SELECT * FROM logins WHERE login_id = ?", [login_id]);
+	return db_conn.query('SELECT * FROM logins WHERE login_id = ?', [login_id]);
 }
 
 export function getLoginByUsername(username) {
-  return db_conn.query("SELECT * FROM logins WHERE login_username = ?", [
-    username,
-  ]);
+	return db_conn.query('SELECT * FROM logins WHERE login_username = ?', [username]);
 }
 
 export function createLogin(username, password) {
-  return db_conn.query(
-    `INSERT INTO logins (login_username, login_password) VALUES (?, ?)`,
-    [username, password]
-  );
+	return db_conn.query(`INSERT INTO logins (login_username, login_password) VALUES (?, ?)`, [username, password]);
 }
 
 export function updateLoginByID(login_id, username, password) {
-  return db_conn.query(
-    `
+	return db_conn.query(
+		`
       UPDATE logins
       SET login_username = ?, login_password = ?
       WHERE login_id = ?
     `,
-    [username, password, login_id]
-  );
+		[username, password, login_id]
+	);
 }
 
 export function deleteLoginByID(login_id) {
-  return db_conn.query("DELETE FROM logins WHERE login_id = ?", [login_id]);
+	return db_conn.query('DELETE FROM logins WHERE login_id = ?', [login_id]);
 }
