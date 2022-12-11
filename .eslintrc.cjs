40021e9ca6092797a95bd6f480f809b12d7eb799
by151709129 <mg86837242@protonmail.com>
2022-12-11 16:34:03 +1000

    checkpoint before running npx prettier --write . to replace tabs

---
+++ .eslintrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+    extends: ['airbnb', 'prettier'],
+    plugins: ['prettier'],
+    ignorePatterns: ['.eslintrc.cjs'],
+    rules: {
+        'prettier/prettier': ['error'],
+    },
+};
