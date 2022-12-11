2ea3937e68282b33f35c119b042a08c3dd744834
by151709129 <mg86837242@protonmail.com>
2022-12-11 19:08:33 +1000

    checkpoint before npx eslint .

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,15 @@
 module.exports = {
     extends: ['airbnb', 'prettier'],
     plugins: ['prettier'],
-    ignorePatterns: ['.eslintrc.cjs'],
+    ignorePatterns: [
+        'src/data/controllers/__controllers_high_street_gym_v0__',
+        'src/data/controllers/__controllers_sail_away__',
+        'src/data/models/__models_high_street_gym_v0__',
+        'src/data/models/__models_sail_away__',
+        'src/data/__database_high_street_gym_v0__.js',
+    ],
     rules: {
         'prettier/prettier': ['error'],
+        indent: ['error', 4],
     },
 };
