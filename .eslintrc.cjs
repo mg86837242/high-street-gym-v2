6c424dc36aadcfa1d0c4e0885c1608a3996aa091
by151709129 <mg86837242@protonmail.com>
2022-12-24 17:44:03 +1000

    disable import/extensions to bypass resolver issue

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,7 +17,7 @@ module.exports = {
     settings: {
         'import/resolver': {
             node: {
-                extensions: ['warn'],
+                extensions: 'warn',
             },
         },
     },
